bfdcfe40806d54e160ca4a8cba81d1ac83af60ff
James Crake-Merani <james@jamescm.co.uk>
2025-09-29 10:34:40 +0100

    Function to load the assessment calendar.

--- src/jamescrake_merani/upcoming_deadlines_script.clj
+++ src/jamescrake_merani/upcoming_deadlines_script.clj
@@ -1,5 +1,19 @@
 (ns jamescrake-merani.upcoming-deadlines-script
+  (:require [dk.ative.docjure.spreadsheet :as ss])
   (:gen-class))
+
+(defn load-assessment-calendar [filename]
+  (->> (ss/load-workbook filename)
+       (ss/select-sheet "Sheet1")
+       (ss/select-columns
+        {:A :module-code
+         :B :module-name
+         :C :assessment-type
+         :D :assessment-name
+         :E :percentage
+         :F :hand-out-date
+         :G :hand-in-date
+         :H :feedback-date})))
 
 (defn greet
   "Callable entry point to the application."

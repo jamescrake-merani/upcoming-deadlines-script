28b5af5fb06dc18c1401955ebd45d43f8dd3a094
James Crake-Merani <james@jamescm.co.uk>
2025-09-29 09:46:03 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to upcoming-deadlines-script
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jamescrake_merani/upcoming_deadlines_script_test.clj
@@ -0,0 +1,7 @@
+(ns jamescrake-merani.upcoming-deadlines-script-test
+  (:require [clojure.test :refer :all]
+            [jamescrake-merani.upcoming-deadlines-script :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
